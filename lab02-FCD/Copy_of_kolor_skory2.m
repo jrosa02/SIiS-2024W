1c1dc9258e84d50be3f8ef70bebd30c06fe60477
Jan Rosa <jrosa@student.agh.edu.pl>
2024-10-16 07:59:43 +0200

    lab 2 face detection

---
+++ lab02-FCD/Copy_of_kolor_skory2.m
@@ -0,0 +1,20 @@
+%% Przygotowanie danych do tworzenia modelu skory
+% CREATED: Jaromir Przybylo, 26.10.2015, R2015a
+%
+addpath('BazaObrazow')
+%% Wybor z obrazu fragmentu zawierajacego wycinek skory
+figure;
+imshow(testowy)
+bw = roipoly;
+% Zaznacz interaktywnie jak najwiekszy fragment twarzy,
+% zawierajacy kolor skory
+% (aby zaakceptowac zaznaczenie, dwukrotnie kliknij 
+%  na zaznaczonym obszarze)
+
+%-zapis do pliku MAT utworzonej maski
+save testowy_0_0000_roibw bw
+
+%% Tworzenie drugiego modelu skory
+model2 = Copy_of_createSkinModel(testowy,bw);
+
+
